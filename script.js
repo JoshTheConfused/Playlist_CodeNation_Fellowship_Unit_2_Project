697d216fdc744e3a7992e829d7b7d99f6e6763e8
JoshTheConfused <97866996+JoshTheConfused@users.noreply.github.com>
2024-01-19 02:24:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let searchButton = document.getElementById("search-button");
+let searchTitle = document.getElementById("title-search");
+let searchArtist = document.getElementById("title-artist");
+let searchLink = document.getElementById("title-link");
