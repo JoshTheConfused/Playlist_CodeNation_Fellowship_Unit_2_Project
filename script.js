b5344c5eef8aca8a68648090f3f2ec0f4b20107f
JoshTheConfused <97866996+JoshTheConfused@users.noreply.github.com>
2024-01-30 00:50:02 +0000

    Added option for album search

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let searchButton = document.getElementById("search-button");
 let searchTitle = document.getElementById("title-search");
-let searchArtist = document.getElementById("title-artist");
-let searchLink = document.getElementById("title-link");
+let searchArtist = document.getElementById("artist-search");
+let searchAlbum = document.getElementById("album-search");
+let searchLink = document.getElementById("link-search");
